bc5862aff9f9a8941f5e79aac1614c903bcb55e0
scooterhacking <hi@scooterhacking.org>
2019-11-02 19:12:39 +0100

    Maintained compatibility for other languages

---
+++ website/static/old_form.js
@@ -0,0 +1,195 @@
+    var forms = {
+        "VERSION": "version",
+        "KERS_MIN_SPEED": "kers_min_speed",
+        "KERS_DIVIDOR": "kers_dividor",
+        "MAX_SPEED": "max_speed",
+        "VERSION_SPOOFING": "version_spoofing",
+        "MOTOR_POWER_CONSTANT": "motor_power_constant",
+        "CRUISE_CONTROL_DELAY": "cruise_control_delay",
+        "THROTTLE_ALG": "throttle_alg",
+        "WHEEL_SPEED_CONST": "wheel_speed_const",
+        "MOTOR_START_SPEED": "motor_start_speed",
+        "REMOVE_CHARGING_MODE": "remove_charging_mode",
+        "STAY_ON_LOCKED": "stay_on_locked",
+        "BMS_UART_76800":"bms_uart_76800",
+        "SWD_ENABLE":"swd_enable",
+        "BYPASS_BMS":"bypass_BMS"
+    };
+
+    var formValues = Object.values(forms);
+    var queryStrings = window.location.search.substring(1);
+    var queries = queryStrings.split('&');
+
+    for (var i = 0; i < queries.length; i++) {
+        var query = queries[i];
+        var tmp = query.split('=');
+        var found = false;
+
+        for (var j = 0; j < formValues.length; j++) {
+            if (formValues[j] === tmp[0]) {
+                found = true;
+                break;
+            }
+        }
+
+        if(found) {
+            if (tmp[1] === 'on') {
+                tmp[1] = true;
+            }
+
+            ChangeForm(tmp[0], tmp[1], true);
+        }
+    }
+
+    function GetForm(name) {
+        return document.getElementsByName(name)[0];
+    }
+
+    function GetFormValue(name) {
+        var o = GetForm(name);
+
+        if (o.type === "checkbox") {
+            return o.checked;
+        }
+
+        return o.value;
+    }
+
+    function GetPatchCheckBox(name) {
+        var form = GetForm(name);
+        return form.nextElementSibling && form.nextElementSibling.children[0];
+    }
+
+    function CheckForm(name, cb) {
+        GetForm(name).disabled = !cb.checked;
+    }
+
+    function ChangeForm(name, value, patch) {
+        var o = GetForm(name);
+
+        if (o.type === "checkbox") {
+            o.checked = value;
+        } else {
+            o.value = value;
+        }
+
+        if (typeof patch === 'boolean') {
+            var cb = GetPatchCheckBox(name);
+
+            if (cb) {
+                cb.checked = patch;
+                cb.onchange();
+            }
+        }
+    }
+
+    function Preset_Default() {
+        ChangeForm(forms.VERSION, "DRV151");
+        ChangeForm(forms.KERS_MIN_SPEED, "6", false);
+        ChangeForm(forms.MAX_SPEED, "32", false);
+        ChangeForm(forms.KERS_DIVIDOR, "2", false);
+        ChangeForm(forms.MOTOR_POWER_CONSTANT, "51575", false);
+        ChangeForm(forms.CRUISE_CONTROL_DELAY, "5", false);
+        ChangeForm(forms.VERSION_SPOOFING, false);
+        ChangeForm(forms.THROTTLE_ALG, false);
+		ChangeForm(forms.REMOVE_CHARGING_MODE, false);
+        ChangeForm(forms.MOTOR_START_SPEED, "5", false);
+        ChangeForm(forms.WHEEL_SPEED_CONST, "390", false);
+        ChangeForm(forms.STAY_ON_LOCKED, false);
+        ChangeForm(forms.BMS_UART_76800, false);
+        ChangeForm(forms.SWD_ENABLE, false);
+        ChangeForm(forms.BYPASS_BMS, false);
+    }
+    function Preset_ES4() {
+        ChangeForm(forms.VERSION, "DRV151");
+        ChangeForm(forms.KERS_MIN_SPEED, "6", false);
+        ChangeForm(forms.MAX_SPEED, "32", true);
+        ChangeForm(forms.KERS_DIVIDOR, "2", false);
+        ChangeForm(forms.MOTOR_POWER_CONSTANT, "51575", false);
+        ChangeForm(forms.CRUISE_CONTROL_DELAY, "5", false);
+        ChangeForm(forms.VERSION_SPOOFING, true);
+        ChangeForm(forms.THROTTLE_ALG, false);
+		ChangeForm(forms.REMOVE_CHARGING_MODE, false);
+        ChangeForm(forms.WHEEL_SPEED_CONST, "390", false);
+        ChangeForm(forms.STAY_ON_LOCKED, true);
+        ChangeForm(forms.BMS_UART_76800, false);
+        ChangeForm(forms.SWD_ENABLE, false);
+        ChangeForm(forms.BYPASS_BMS, false);
+    }
+    function Preset_ES4L() {
+        ChangeForm(forms.VERSION, "DRV151");
+        ChangeForm(forms.KERS_MIN_SPEED, "6", false);
+        ChangeForm(forms.MAX_SPEED, "27", true);
+        ChangeForm(forms.KERS_DIVIDOR, "2", false);
+        ChangeForm(forms.MOTOR_POWER_CONSTANT, "51575", false);
+        ChangeForm(forms.CRUISE_CONTROL_DELAY, "5", false);
+        ChangeForm(forms.VERSION_SPOOFING, true);
+        ChangeForm(forms.THROTTLE_ALG, false);
+		ChangeForm(forms.REMOVE_CHARGING_MODE, false);
+        ChangeForm(forms.WHEEL_SPEED_CONST, "390", false);
+        ChangeForm(forms.STAY_ON_LOCKED, true);
+        ChangeForm(forms.BMS_UART_76800, false);
+        ChangeForm(forms.SWD_ENABLE, false);
+        ChangeForm(forms.BYPASS_BMS, false);
+    }
+    function Preset_ES2Mod() {
+        ChangeForm(forms.VERSION, "DRV139");
+        ChangeForm(forms.KERS_MIN_SPEED, "6", false);
+        ChangeForm(forms.MAX_SPEED, "28", true);
+        ChangeForm(forms.KERS_DIVIDOR, "2", false);
+        ChangeForm(forms.MOTOR_POWER_CONSTANT, "45000", true);
+        ChangeForm(forms.CRUISE_CONTROL_DELAY, "5", false);
+        ChangeForm(forms.VERSION_SPOOFING, true);
+        ChangeForm(forms.THROTTLE_ALG, false);
+		ChangeForm(forms.REMOVE_CHARGING_MODE, false);
+        ChangeForm(forms.WHEEL_SPEED_CONST, "390", false);
+        ChangeForm(forms.STAY_ON_LOCKED, true);
+        ChangeForm(forms.BMS_UART_76800, false);
+        ChangeForm(forms.SWD_ENABLE, false);
+        ChangeForm(forms.BYPASS_BMS, false);
+    }
+    function Share() {
+        var url = location.protocol + '//' + location.host;
+        var firstParam = true;
+
+        function getSeparator() {
+            if (firstParam) {
+                firstParam = false;
+                return '?';
+            }
+
+            return '&';
+        }
+
+        for (var i = 0; i < formValues.length; i++) {
+            var form = formValues[i];
+
+            var formValue = GetFormValue(form);
+            var patchCheckbox = GetPatchCheckBox(form);
+
+            if (patchCheckbox) {
+                if (patchCheckbox.checked) {
+                    url += getSeparator() + form + '=' + formValue;
+                }
+            } else if (typeof formValue === 'boolean') {
+                if (formValue) {
+                    url += getSeparator() + form + '=on';
+                }
+            } else {
+                url += getSeparator() + form + '=' + formValue;
+            }
+        }
+
+        var textArea = document.createElement("textarea");
+
+        textArea.value = url;
+        document.body.appendChild(textArea);
+
+        textArea.focus();
+        textArea.select();
+
+        document.execCommand('copy');
+        document.getElementById('shareConfirmation').innerText = 'Copied!';
+
+        document.body.removeChild(textArea);
+    }
